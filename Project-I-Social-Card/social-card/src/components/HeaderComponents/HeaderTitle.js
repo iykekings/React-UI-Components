87cbfb1fd558d2c538942863356f84f0349422c5
Ikechukwu Eze <iykekings36@gmail.com>
2019-05-27 16:13:02 +0100

    Done ImageThumbnail and HeaderTitle component

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+  return (
+    <div>
+      <h3 className="title">Lambda School</h3>
+      <span>
+        @lambdaSchool *
+        {Date(Date.now)
+          .match(/\s(?:\w+\s\d+)\s/)[0]
+          .trim()}
+      </span>
+    </div>
+  );
+};
+
+export default HeaderTitle;
