42b7f08514779c2861cb96695b7ea7bd7aacdac9
Ikechukwu Eze <iykekings36@gmail.com>
2019-05-27 17:00:48 +0100

    Add cardContainer and CardContent

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Card.css';
 
 const CardBanner = () => {
-  return <div className="card-banner" />;
+  return (<div className="card-banner"></div>);
 };
 
 export default CardBanner;
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,15 @@
 import React from 'react';
+import CardBanner from './CardBanner';
+import CardContent from './CardContent';
 import './Card.css';
+
+const CardContainer = () => {
+  return (
+    <a className="card-container" href="https://www.reactjs.org">
+      <CardBanner />
+      <CardContent />
+    </a>
+  );
+};
+
+export default CardContainer;
