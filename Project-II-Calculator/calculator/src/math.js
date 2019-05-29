e57402f32f16dbc75e6503fa2fc9d0387834031d
Ikechukwu Eze <iykekings36@gmail.com>
2019-05-29 18:41:07 +0100

    add comment to math.js

--- Project-II-Calculator/calculator/src/math.js
+++ Project-II-Calculator/calculator/src/math.js
@@ -7,19 +7,34 @@ export default function calc(expression) {
       let [first, second] = current.match(matchExp)[0].split(type);
       switch (type) {
         case '/':
-          current = current.replace(matchExp, `${parseInt(first) / parseInt(second)}`);
+          current = current.replace(
+            matchExp,
+            `${parseInt(first) / parseInt(second)}`
+          );
           break;
         case 'x':
-          current = current.replace(matchExp, `${parseInt(first) * parseInt(second)}`);
+          current = current.replace(
+            matchExp,
+            `${parseInt(first) * parseInt(second)}`
+          );
           break;
         case '+':
-          current = current.replace(matchExp, `${parseInt(first) + parseInt(second)}`);
+          current = current.replace(
+            matchExp,
+            `${parseInt(first) + parseInt(second)}`
+          );
           break;
         case '-':
-          current = current.replace(matchExp, `${parseInt(first) - parseInt(second)}`);
+          current = current.replace(
+            matchExp,
+            `${parseInt(first) - parseInt(second)}`
+          );
           break;
         default:
           current = +current;
+        // Then include the logic here like so, (before the default case!)
+        // case '%':
+        //    current = current.replace(matchExp, `${parseInt(first) % parseInt(second)}`)
       }
     }
   }
@@ -27,5 +42,8 @@ export default function calc(expression) {
   arith('x');
   arith('+');
   arith('-');
+
+  // You can extend the functionality of this function by callind arith with the symbol of operation and including the logic in the switch statement, e.g, arith('%') (NB: the arith calls should be arranged in other JS operator precedence https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence);
+
   return current;
 }
